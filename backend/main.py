558dbdeaa89a2f6f5001cfb4d46bcd6061fa476b
vivaanm867 <vivaanyt07@gmail.com>
2026-05-04 14:39:41 -0400

    basic backend server running

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+  return {
+    "message": "NutriScan backend is working"
+  }
